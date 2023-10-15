f170a1fa01bf288715a957abbfd21ff2ce5a4493
Md. Jamal Uddin <jaamaalxyz@gmail.com>
2023-10-15 12:12:27 +0600

    Add for loops playground

--- playgrounds/Loops.playground/Contents.swift
+++ playgrounds/Loops.playground/Contents.swift
@@ -1,17 +1,23 @@
 import UIKit
 
-let levels = 10
-let freeLevels = 4
-let bonusLevel = 3
+let levelScores = [10, 20, 30, 40, 50, 60, 70]
+for (level, score) in levelScores.enumerated() {
+    print("The score of the level \(level + 1) is \(score).")
+}
 
-for level in 1...levels {
-  if level == bonusLevel {
-    print("Skip bonus level \(bonusLevel).")
-    continue
-  }
-  print("Play level \(level).")
-  if level == freeLevels {
-    print("You have played all \(freeLevels) free levels. Buy the game to play the remaining \(levels - freeLevels) levels.")
-    break
-  }
+var gameScore = 0
+for level in levelScores {
+    gameScore += level
+}
+print("The output should display that the final game score is \(gameScore).")
+
+let weeklyTemperatures = ["Monday": 70, "Tuesday": 75, "Wednesday": 80, "Thursday": 85, "Friday": 90, "Saturday": 95, "Sunday": 100]
+for (day, temperature) in weeklyTemperatures {
+    print("The temperature of \(day) is \(temperature).")
+}
+
+let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+let temperatures = [70, 75, 80, 85, 90, 95, 100]
+for (level, day) in days.enumerated() {
+    print("The temperature of the \(day) is \(temperatures[level])°F.")
 }
